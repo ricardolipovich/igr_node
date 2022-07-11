225bf431fef2c9745d1dfb5b55f9761e1a7aaaaf
Ricardo Lipovich <ricardolipovich@gmail.com>
2022-07-10 22:27:24 -0300

    funcionamiento del login

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var usuarioModel = require('./../../models/usuariosModel');
 
 router.get('/', function(req, res, next) {
     res.render('admin/login',{
         layout:'admin/layout'
     });
-  })
+  });
+
+  router.post('/', async function(req, res, next) {
+    try{
+      console.log(req.body);
+      var usuario = req.body.usuario;
+      var password = req.body.password;
+
+      var data = await usuarioModel.getUserAndPassword(usuario,password);
+
+      if(data != undefined){
+        res.redirect('/admin/novedades')
+      }else{
+        res.render('admin/login',{
+          layout:'admin/layout',
+          error:true
+        })
+      }
+
+
+    }catch (error){
+      console.log(error)
+    }
+  });
 
 
 module.exports = router;
