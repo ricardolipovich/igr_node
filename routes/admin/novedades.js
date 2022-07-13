10e3f1300f7f7512d6049812bacc0e9d73844ea8
Ricardo Lipovich <ricardolipovich@gmail.com>
2022-07-12 22:55:50 -0300

    tabla novedades -listado

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req,res,netx){
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades',{
-        layout:'admin/layout',
-        persona:req.session.nombre
+        layout:'admin/layout', 
+        persona:req.session.nombre, novedades 
+        
+        // admin/layout.hbs
     });
-  });
+    
+});
 
-  module.exports = router;
+
+
+module.exports = router;
