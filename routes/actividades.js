b69082ae4e93a72dd61b2c19376915f6b341111d
Ricardo Lipovich <ricardolipovich@gmail.com>
2022-07-25 06:59:29 -0300

    actividades desde el front

--- routes/actividades.js
+++ routes/actividades.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../models/novedadesModel');
 
-router.get('/', function(req, res, next) {
-    res.render('actividades', { isActividades: true });
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
+  res.render('actividades', {
+    isActividades: true,
+    novedades
   });
+});
 module.exports = router;
